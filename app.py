3c9f0fbc6fa9569d897bbbda6d755e44f6f062de
Luís Miguel <luismiguelpessoal1@gmail.com>
2025-11-16 16:37:22 -0300

    Definindo variáveis como númerica

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ st.title("Dashboard Exploratório da Pesquisa")
 def load_data(filepath):
     try:
         df = pd.read_csv(filepath)
-        return df
+        cols_numericas = ['horas_estudo_semanal', 'tempo_deslocamento_min', 'semestre', 'nivel_disposição_pos_aula']
+        for col in cols_numericas:
+            if col in df.columns:
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+        return df.dropna(subset=cols_numericas)
     except FileNotFoundError:
         return None
 
