0ff6c4ddd34e93f987630680f4e0ec7359eb7fb8
Luís Miguel <luismiguelpessoal1@gmail.com>
2025-11-23 12:53:54 -0300

    Alterações de estética

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import statsmodels.api as sm
 
 st.set_page_config(page_title="Dashboard Exploratório", layout="wide")
+st.title("Análises do tempo de deslocamento dos estudantes da UFC")
 
 @st.cache_data
 def load_data(filepath):
     try:
         df = pd.read_csv(filepath)
-        cols_numericas = ['horas_estudo_semanal', 'tempo_deslocamento_min', 'semestre', 'nivel_disposição_pos_aula']
+        cols_numericas = ['horas_estudo_semanal', 'tempo_deslocamento_min', 'semestre', 'nivel_disposicao_pos_aula']
         for col in cols_numericas:
             if col in df.columns:
                 df[col] = pd.to_numeric(df[col], errors='coerce')
@@ -58,19 +60,19 @@ if df is not None:
 
     with col1:
         st.subheader("Distribuição dos Dados")
-        var_analise = st.selectbox("Variável:", options=['horas_estudo_semanal', 'tempo_deslocamento_min', 'nivel_disposição_pos_aula'])
+        var_analise = st.selectbox("Variável:", options=['horas_estudo_semanal', 'tempo_deslocamento_min', 'nivel_disposicao_pos_aula'])
         fig_hist = px.histogram(df_filtrado, x=var_analise, nbins=20, title=f"Histograma: {var_analise}")
         st.plotly_chart(fig_hist, use_container_width=True)
 
     with col2:
         st.subheader("Relação: Deslocamento vs. Disposição")
         fig_scatter = px.scatter(
-            df_filtrado, x='tempo_deslocamento_min', y='nivel_disposição_pos_aula',
+            df_filtrado, x='tempo_deslocamento_min', y='nivel_disposicao_pos_aula',
             color='curso', size='horas_estudo_semanal', trendline="ols"
         )
         st.plotly_chart(fig_scatter, use_container_width=True)
 
-        with st.expander("Visualizar Dados"):
-            st.dataframe(df_filtrado)
+    with st.expander("Visualizar Dados"):
+        st.dataframe(df_filtrado)
 else:
     st.error("Arquivo 'dados.csv' não encontrado.")
